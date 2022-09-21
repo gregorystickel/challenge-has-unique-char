947ea52d66c79d5c994f8db580ad7e96d43860f6
Greg Stickel <gregory.stickel@live.com>
2022-09-21 09:23:13 -0400

    Completed

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,13 @@
 // Write your code below
+
+
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    console.log(uniqueChars);
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    console.log(uniqueChars);
+    return uniqueChars.size === word.length
+  }
+  console.log(hasUniqueChars(`Moonday`));
